1f374b1a4c0040f457a7a71a82b3624ceff20987
Jinhe Zhang <raidene1@outlook.com>
2025-07-03 15:03:20 -0400

    papi

--- kstream/src/main/java/io/confluent/developer/WordCount.java
+++ kstream/src/main/java/io/confluent/developer/WordCount.java
@@ -13,13 +13,20 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.processor.api.Processor;
+import org.apache.kafka.streams.processor.api.ProcessorContext;
+import org.apache.kafka.streams.processor.api.Record;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.StoreBuilder;
+import org.apache.kafka.streams.state.Stores;
 
 /**
- * Demonstrates, using the high-level KStream DSL, how to implement the WordCount program
+ * Demonstrates, using the low-level Processor API, how to implement the WordCount program
  * that computes a simple word occurrence histogram from an input text.
  * <p>
  * In this example, the input stream reads from a topic named "streams-plaintext-input", where the values of messages
@@ -51,7 +58,7 @@ public class WordCount {
         return config;
     }
 
-    static   void createWordCountStream(final StreamsBuilder builder) {
+    static void createWordCountStream(final StreamsBuilder builder) {
         KStream<String, String> source = builder.stream(INPUT_TOPIC);
 
         final KTable<String, Long> counts = source
@@ -67,9 +74,63 @@ public class WordCount {
                 System.out.println("Produced: key=" + word + ", value=" + count);
             })
             .to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.Long()));
+    }
 
-        // need to override value serde to Long type
-        counts.toStream().to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.Long()));
+    public static class WordCountProcessor implements Processor<String, String, String, Long> {
+        private KeyValueStore<String, Long> kvStore;
+        private ProcessorContext<String, Long> context;
+        private final String storeName = "Counts";
+
+        @Override
+        public void init(ProcessorContext<String, Long> context) {
+            this.context = context;
+            this.kvStore = context.getStateStore(storeName);
+        }
+
+        @Override
+        public void process(Record<String, String> record) {
+            System.out.println("Consumed: key=" + record.key() + ", value=" + record.value());
+            final String[] words = record.value().toLowerCase(Locale.getDefault()).split("\\W+");
+
+            for (final String word : words) {
+                if(word.isEmpty()) continue;
+
+                Long oldCount = this.kvStore.get(word);
+                if (oldCount == null) {
+                    oldCount = 0L;
+                }
+                final Long newCount = oldCount + 1;
+                this.kvStore.put(word, newCount);
+
+                System.out.println("Produced: key=" + word + ", value=" + newCount);
+                context.forward(new Record<>(word, newCount, record.timestamp()));
+            }
+        }
+
+        @Override
+        public void close() {
+        }
+    }
+
+    static Topology createWordCountTopology() {
+        final Topology topology = new Topology();
+        final String sourceName = "Source";
+        final String processorName = "Process";
+        final String sinkName = "Sink";
+        final String stateStoreName = "Counts";
+
+        topology.addSource(sourceName, INPUT_TOPIC);
+        topology.addProcessor(processorName, WordCountProcessor::new, sourceName);
+
+        final StoreBuilder<KeyValueStore<String, Long>> countStoreBuilder =
+                Stores.keyValueStoreBuilder(
+                        Stores.persistentKeyValueStore(stateStoreName),
+                        Serdes.String(),
+                        Serdes.Long());
+
+        topology.addStateStore(countStoreBuilder, processorName);
+        topology.addSink(sinkName, OUTPUT_TOPIC, Serdes.String().serializer(), Serdes.Long().serializer(), processorName);
+        return topology;
     }
 
     public static void main(final String[] args) throws IOException {
@@ -99,9 +160,14 @@ public class WordCount {
             put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         }};
 
-        final StreamsBuilder builder = new StreamsBuilder();
-        createWordCountStream(builder);
-        final KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        // final StreamsBuilder builder = new StreamsBuilder();
+        // createWordCountStream(builder);
+        // final KafkaStreams streams = new KafkaStreams(builder.build(), props);
+
+        // To run the PAPI version, comment out the three lines above and uncomment the two lines below
+        final Topology topology = createWordCountTopology();
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+
         final CountDownLatch latch = new CountDownLatch(1);
 
         streams.setStateListener((newState, oldState) -> {
